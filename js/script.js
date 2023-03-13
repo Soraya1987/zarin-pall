4705b96114de109db449718d17025974fc3d6ff7
Soraya Mahmoudi <Soraya_mahmoudi95@yahoo.com>
2023-03-13 09:48:25 +0330

    added navbar drop down

--- js/script.js
+++ js/script.js
@@ -9,4 +9,22 @@ hamburgerIcon.addEventListener('click',function(){
 });
 crossIcon.addEventListener('click',function(){
     hamburgerMenu.classList.remove('show-hamburger-menu')
-})
+});
+showMoreMenu.addEventListener('mouseenter',function(e){
+    const ul=e.target.childNodes[5];
+    ul.style.animation='showUp 0.35s 1';
+    ul.style.display='block'
+});
+showMoreMenu.addEventListener('mouseleave',function(e){
+    const ul=e.target.childNodes[5];
+    ul.style.display='none';
+});
+showProductMenu.addEventListener('mouseenter',function(e){
+    const ul=e.target.childNodes[5];
+    ul.style.animation='showUp 0.35s 1';
+    ul.style.display='flex'
+});
+showProductMenu.addEventListener('mouseleave',function(e){
+    const ul=e.target.childNodes[5];
+    ul.style.display='none';
+});
